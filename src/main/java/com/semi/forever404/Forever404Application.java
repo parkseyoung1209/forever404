69b216c151df6b58473e5436c17e6e3d4048e3e6
610sky <dbsdbwls1999@gmail.com>
2024-07-30 16:41:00 +0900

    Initial commit

---
+++ src/main/java/com/semi/forever404/Forever404Application.java
@@ -0,0 +1,13 @@
+package com.semi.forever404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Forever404Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Forever404Application.class, args);
+	}
+
+}
