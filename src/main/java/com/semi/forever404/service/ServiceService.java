84338d21bb41a52f07d1c064a9ee5503993e64b9
parkseyoung1209 <rodlsdyd4@naver.com>
2024-08-23 09:34:44 +0900

    0823_update

--- src/main/java/com/semi/forever404/service/ServiceService.java
+++ src/main/java/com/semi/forever404/service/ServiceService.java
@@ -57,29 +57,7 @@ public class ServiceService {
         String imageSrc = imageElement.getAttribute("src"); // 이미지의 src 속성 값 가져오기
         System.out.println("Image URL: " + imageSrc);
         driver.close();
-			
-				Thread.sleep(1500);
-				
-				List<WebElement> tmp = driver.findElements(By.cssSelector("#hdtb-sc > div > div.qogDvd > div.crJ18e > div > div"));
-				
-				for(WebElement w : tmp) {
-					if(w.getText().equals("이미지")) {
-						w.click();
-						break;
-					}
-				}
-				
-				WebElement tmp2 = driver.findElement(By.xpath("//*[@id=\"rso\"]/div/div/div[1]/div/div/div[1]/div[2]/h3/a/div/div/div/g-img"));
-				tmp2.click();
-				
-				WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1500));
-				WebElement popupElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"Sva75c\"]/div[2]/div[2]/div/div[2]/c-wiz/div/div[3]/div[1]/a")));
-				
-				WebElement imageElement = popupElement.findElement(By.tagName("img")); // 팝업 내에서 이미지 요소 찾기
-		        String imageSrc = imageElement.getAttribute("src"); // 이미지의 src 속성 값 가져오기
-		        System.out.println("Image URL: " + imageSrc);
-		        driver.close();
-		        Thread.sleep(1500);
-		        return imageSrc;
+        Thread.sleep(1500);
+        return imageSrc;
 	}
 }
