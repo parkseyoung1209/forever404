c19e1c3fdaf10a87fdb7937bb531f24b13d8ae52
parkseyoung1209 <rodlsdyd4@naver.com>
2024-09-03 17:35:10 +0900

    0903-최종본

--- src/main/resources/static/js/detail2.js
+++ src/main/resources/static/js/detail2.js
@@ -4,11 +4,35 @@ document.addEventListener("DOMContentLoaded", function() {
 	const items = document.querySelectorAll(".carousel-item");
 	let currentIndex = 1;
 
+	const curDateNodes = document.querySelectorAll(".h1date");
+	let curDate;
+	
+	curDateNodes.forEach((curDate)=>{
+		if(curDate.isEqualNode(curDateNodes[currentIndex-1])) {
+			curDate=curDate.getAttribute("id");
+			sessionStorage.setItem("curDate",curDate);
+			console.log(curDate);
+		}
+	});
+	
+	/*
+	const array = temp.map(function(value) {
+		console.log(value);
+	});
+	*/
+	
 	prevButton.addEventListener("click", function() {
 		if (currentIndex > 1) {
 			$(`.carousel-item:nth-child(${currentIndex - 1})`).css({ display: 'block' });
 			$(`.carousel-item:nth-child(${currentIndex})`).css({ display: 'none' });
 			currentIndex--;
+			curDateNodes.forEach((curDate)=>{
+				if(curDate.isEqualNode(curDateNodes[currentIndex-1])) {
+					curDate=curDate.getAttribute("id");
+					sessionStorage.setItem("curDate",curDate);
+					console.log(curDate);
+				}	
+			});
 		}
 	});
 
@@ -17,6 +41,13 @@ document.addEventListener("DOMContentLoaded", function() {
 			$(`.carousel-item:nth-child(${currentIndex})`).css({ display: 'none' });
 			$(`.carousel-item:nth-child(${currentIndex + 1})`).css({ display: 'block' });
 			currentIndex++;
+			curDateNodes.forEach((curDate)=>{
+				if(curDate.isEqualNode(curDateNodes[currentIndex-1])) {
+					curDate=curDate.getAttribute("id");
+					sessionStorage.setItem("curDate",curDate);
+					console.log(curDate);
+				}	
+			});
 		}
 	});
 });
